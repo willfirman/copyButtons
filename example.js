187741b2a1cc8046bc98a1442320180dda21fb85
willfirman <61538077+willfirman@users.noreply.github.com>
2022-06-24 08:54:21 +0100

    Delete example.js

--- example.js
+++
@@ -1,5 +0,0 @@
-import {COPY_BUTTON_CLASSES} from './copy_buttons';
-console.log('add:');
-console.log(COPY_BUTTON_CLASSES.success.add);
-COPY_BUTTON_CLASSES.success.add = ['an added class']
-console.log('add now:');
